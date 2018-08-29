1778ca20f6baf7842ef538e0589ab0ea76be48f8
Waywrong <wlhsu@ira.ee.ntu.edu.tw>
2018-08-29 16:39:50 +0800

    animation

--- example/Errorbar.cpp
+++ example/Errorbar.cpp
@@ -6,20 +6,32 @@ namespace plt = matplotlibcpp;
 
 int main() 
 {  
-	int n = 80;
-  vector<double> x(n),y(n);
-  vector<double> xErr(n),yErr(n);
-  for(int i=0;i<n;i++)
+	vector<double> x,y;
+  vector<double> xErr,yErr;
+  int initNum = 50;
+  for(int i=0;i<initNum;i++)
   {
-  	x.at(i) = 0.5*i;
-  	y.at(i) = i;
-  	xErr.at(i) = .3;
-  	yErr.at(i) = .3*i;
+  	x.push_back(0.5*i);
+  	y.push_back(i);
+  	xErr.push_back(3);
+  	yErr.push_back(.3*i);
   }
-  plt::title("errorbar");
-  plt::errorbar(x,y,yErr);
-  //plt::plot(x,y,"r--");
-  plt::named_plot("path",x,y,"r--");
-  plt::legend();
-  plt::show();
+
+  int n=10;
+  for(int k=0;k<n;k++)
+  {
+  	x.push_back(0.2*k+0.5*initNum);
+  	y.push_back(k+initNum);
+  	xErr.push_back(7);
+  	yErr.push_back(.7*k);
+
+  	plt::clf();
+	  plt::title("errorBar");
+	  plt::errorbar(x,y,yErr);
+	  plt::named_plot("path",x,y,"r--");
+	  //plt::xlim(0, n*n);
+	  plt::legend();
+	  plt::pause(0.01);
+	}
+	plt::show();
 } 

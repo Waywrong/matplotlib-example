681adb2643128ec053a3776136cca166d0bf97d4
Waywrong <wlhsu@ira.ee.ntu.edu.tw>
2018-09-04 15:14:24 +0800

    subplot

--- example/histogram.cpp
+++ example/histogram.cpp
@@ -8,7 +8,7 @@ int main()
 {  
 	vector<double> x,y;
   vector<double> xErr,yErr;
-  int initNum = 500000;
+  int initNum = 500;
   srand(time(NULL));
   for(int i=0;i<initNum;i++)
   {
@@ -17,8 +17,24 @@ int main()
   }
 
   plt::clf();
+  plt::subplot(3,2,1);
   plt::title("histogram");
   plt::hist(y,50);
+  plt::ylabel("count");
+
+  plt::subplot(3,2,2);
+  plt::title("plot_bo");
+  plt::plot(x,y,"bo");
+
+  plt::subplot(3,2,4);
+  plt::title("plot_r--");
+  plt::plot(x,y,"r--");
+
+  plt::subplot(3,2,5);
+  plt::title("plot_gx");
+  plt::plot(x,y,"gx");
+  plt::xlabel("time");
+
   plt::legend();
   plt::show();
 } 
